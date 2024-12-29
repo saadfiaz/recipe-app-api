41698ccb23914034b196cdb16ea731e03664808b
saadfiaz <saadfiaz48@gmail.com>
2024-12-29 20:20:08 +0500

    Configured docker-compose and checks wait_for_db again

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,11 +5,11 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_numbers(self):
         """Test Adding numbers"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_subtract_number(self):
         """Test Subtracting number"""
